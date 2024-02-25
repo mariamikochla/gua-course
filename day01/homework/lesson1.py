299eebab61137de2bd705b885464199da324bca4
mariamikochla <kochlamazashvilimariam95@gmail.com>
2024-02-25 20:22:06 +0400

    davaleba

---
+++ day01/homework/lesson1.py
@@ -0,0 +1,21 @@
+"მარიამი" #string str
+
+150 #integer int
+
+2.3  #float 
+
+print("mariami")
+
+
+
+name = "my name is mariam"
+surname = "kochlamazashvili"
+age = "i'm 10 years old"
+Height = "i'm 1.50 metres tall"
+story = "I entered the goal oriented academy because i want to learn such a future proffesion as programing"
+
+
+print(name + " " + surname +" " + age + " " + Height + " " + story )
+
+
+
